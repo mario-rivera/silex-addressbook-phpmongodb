b12668c78332524c96aa9c1bc144698cdc41a458
mario <mario@studyportals.eu>
2017-09-17 21:51:31 +0200

    added mongodm as a service

--- app/Services/definitions.php
+++ app/Services/definitions.php
@@ -1,5 +1,6 @@
 <?php
 return [
     App\Routing\RouteServiceProvider::class,
-    App\Services\Twig\TwigServiceProvider::class
+    App\Services\Twig\TwigServiceProvider::class,
+    App\Services\MongoDM\MongodmServiceProvider::class
 ];
